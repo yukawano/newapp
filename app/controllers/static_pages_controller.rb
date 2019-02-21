7daa4367297f71aee8aa8d9c3508febdb0052d67
kkk <yukawano@gmail.com>
2019-02-21 22:47:14 +0900

    p

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    # app/views/リソース名/アクション名.html.erb
-    # app/views/static_pages/home.html.erb
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
